bb78dc192cfb75afa0bbf1b39c80a5ec3d93a1ba
Beachnad <DannyBeachnau@gmail.com>
2020-02-14 00:04:39 -0500

    Fix the deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,2 @@
-database_exists=$(psql -ltq | cut -d \| -f 1 | grep -w 'airflow')
-
-if ${database_exists}
-then
-  echo "It exists"
-else
-  echo "Nope..."
-fi
+#!/bin/sh
+docker-compose up --no-deps flyway
